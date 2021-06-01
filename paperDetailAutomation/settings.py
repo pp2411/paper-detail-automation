4ae73583e0195fb82ae404ae96ea4fd1085d6cab
Prajwal Pandey <nickey.24nov@gmail.com>
2021-06-01 19:11:26 +0530

    added django_heroku

--- paperDetailAutomation/settings.py
+++ paperDetailAutomation/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,3 +141,4 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
+django_heroku.settings(locals())
